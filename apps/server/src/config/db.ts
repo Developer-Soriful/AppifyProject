e197b6a590ded1591fe62fc3a04910c7ee3f55e8
Developer-Soriful <93891396soriful@gmail.com>
2026-04-05 14:08:09 +0600

    resolve

--- apps/server/src/config/db.ts
+++ apps/server/src/config/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error("MONGO_URI is not defined");
+  const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGO_URI or MONGODB_URI is not defined");
 
   await mongoose.connect(uri, { maxPoolSize: 10 });
   console.log(`MongoDB connected: ${mongoose.connection.host}`);
